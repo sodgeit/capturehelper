24f9a6566e050b2e79422c56fab7d6609517dcee
David Tahir <david.tahir@sodgeit.de>
2021-12-21 14:17:25 +0100

    added new launch parameter --hide-trayicon

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -9582,7 +9582,11 @@ void OBSBasic::SystemTrayInit()
 	trayMenu->addSeparator();
 	trayMenu->addAction(exit);
 	trayIcon->setContextMenu(trayMenu);
-	trayIcon->show();
+	if(opt_hide_trayicon) {
+		trayIcon->hide();
+	} else {
+		trayIcon->show();
+	}
 
 	if (outputHandler && !outputHandler->replayBuffer)
 		sysTrayReplayBuffer->setEnabled(false);
@@ -9656,9 +9660,7 @@ void OBSBasic::SystemTray(bool firstStarted)
 	if (!sysTrayEnabled) {
 		trayIcon->hide();
 	} else {
-		if(true) {
-			trayIcon->hide();
-		} else {
+		if(!opt_hide_trayicon) {
 			trayIcon->show();
 		}
 		if (firstStarted && (sysTrayWhenStarted || opt_minimize_tray)) {
